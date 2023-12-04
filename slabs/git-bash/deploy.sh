42f02dac1c6a39eead51ffd88e987e51ac768aef
stvnrs <steven@eightsix.io>
2023-12-04 14:31:47 +0000

    add git-bash slab

---
+++ slabs/git-bash/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+FILE_NAME="git-prompt.sh"
+SOURCE_FILE_PATH="$(dirname $0)/$FILE_NAME"
+TARGET_PATH="$HOME/.config/git/"
+echo $SOURCE_FILE_PATH
+echo $TARGET_PATH
+cp "$SOURCE_FILE_PATH" "$TARGET_PATH"
