fea5389bec2b89ae5b03363cce9998e540ca0efb
Steven Rose <steven@eightsix.io>
2023-12-11 21:22:19 +0000

    added dynamic params for Deploy-Slab
    adds params from the named slab to the Deploy-Slab cmdlet

--- slabs/pwsh-profiles/lay.ps1
+++ slabs/pwsh-profiles/lay.ps1
@@ -13,12 +13,21 @@ $SourcePath = Join-Path $PSScriptRoot 'src' '*.ps1'
 $TargetPath =  Join-Path $HOME 'powershell' 'profile' 
 
 if(!(Test-Path $TargetPath)){
-    mkdir $TargetPath 
+    $InfoMessage = "Creating profile directory $(emph $TargetPath)"
+    Write-Information "INFO: $InfoMessage$LogStart"
+    mkdir $TargetPath | Out-Null
+    Write-Information "INFO: $InfoMessage$LogDone"
 }
 
+$InfoMessage = "Copying profiles to $(emph $TargetPath)"
+Write-Information "INFO: $InfoMessage$LogStart"
 Copy-Item $SourcePath $TargetPath
+Write-Information "INFO: $InfoMessage$LogDone"
 
 if($DeploySynchedProfileShim.IsPresent){
+    $InfoMessage = "Deploying synched-profile-shim.ps1 to $PROFILE"
+    Write-Information "INFO: $InfoMessage$LogStart"
     Copy-Item (Join-Path $PSScriptRoot 'src' 'synched-profile-shim.ps1') $PROFILE
+    Write-Information "INFO: $InfoMessage$LogDone"
 }
 
--- test-deploy.ps1
+++ test-deploy.ps1
@@ -1,8 +1,10 @@
 
+
+
 $SetUp = {
-    import-Module .\pwsh\pave.psm1 -Force
+    Import-Module .\pwsh\pave.psm1 -Force
     $Cache = Join-Path ([System.IO.Path]::GetTempPath()) ([System.IO.Path]::GetRandomFileName())
-    New-Item -ItemType Directory -Path $Cache | Out-Null
+    New-Item -ItemType Directory -Path $Cache 
     Set-Cache $Cache
     Set-Remote C:\Users\StevenRose\source\repos\pave\build\
     Find-Slab | Install-Slab
@@ -10,8 +12,21 @@ $SetUp = {
 }
 
 $TearDown = {
-    Remove-Item -Path $Cache -Recurse -Force
+    Remove-Item -Path (Get-Cache) -Recurse -Force -Verbose
+    $ErrorActionPreference = $CurrentErrorActionPreference
+    $InformationPreference = $CurrentInformationPreference
+    $VerbosePreference = $CurrentVerbosePreference
+    Read-Host -Prompt 'Press any key to exit'
+    exit
 }
 
 $Setup.Invoke()
-Deploy-Slab bs
+
+$CurrentErrorActionPreference = $ErrorActionPreference
+$CurrentInformationPreference = $InformationPreference
+$CurrentVerbosePreference = $VerbosePreference 
+
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+$VerbosePreference = 'Continue'
+# Deploy-Slab bs
