b2007a703fd52db91a8ae912eda288be844e92bb
steven <you@example.com>
2023-12-22 11:08:33 -0500

    updated reg-tweaks slab

--- slabs/reg-tweaks/lay.ps1
+++ slabs/reg-tweaks/lay.ps1
@@ -1,40 +1,74 @@
 
+param (
+    [Parameter(ParameterSetName = "Default", Mandatory = $false)]
+    [string[]]$Include = @(),
+
+    [Parameter(ParameterSetName = "Default", Mandatory = $false)]
+    [string[]]$Exclude = @(),
+
+    [Parameter(ParameterSetName = "ListAvailable", Mandatory = $true)]
+    [switch]$ListAvailable,
+
+    [Parameter(ParameterSetName = "ListAvailable", Mandatory = $false)]
+    [switch]$Detailed,
+
+    [Parameter(ParameterSetName = "Default", Mandatory = $false )]
+    [Parameter(ParameterSetName = "ListAvailable", Mandatory = $false)]
+    [string]$TweaksPath = "$PSScriptRoot\tweaks"
+
+)
+
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
-$VerbosePreference = 'Continue'
-$WarningPreference = 'Continue'
 
 $SlabsRoot = (Resolve-Path(Join-Path $PSScriptRoot '..')).Path
 . "$SlabsRoot/slab-utils/slab-utils.ps1"
 
-# windows themes settings
-$Path = 'HKCU:Software\Microsoft\Windows\CurrentVersion\Themes\Personalize'
-sp -path $Path -Name 'AppsUseLightTheme' -Value 0
-sp -path $Path -Name 'SystemUsesLightTheme' -Value 0
+$TweakList = Get-ChildItem "$TweaksPath\*.reg-tweaks.json"
 
-# explorer settings
-$Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+switch ($PSCmdlet.ParameterSetName) {
+    'ListAvailable' {
+        $TweakList | ForEach-Object {
+            $Tweak = Get-Content -Raw $_.FullName | ConvertFrom-Json
 
-# taskbar settings
-sp -path $Path -Name 'ShowTaskViewButton' -Value 0 -Type 'DWord'
-sp -path $Path -Name 'TaskbarDa' -Value 0 -Type 'DWord' # widgets
-sp -path $Path -Name 'TaskbarMn' -Value 0 -Type 'DWord' # chat
-sp -path $Path -Name 'TaskbarSi' -Value 1 -Type 'DWord' # taskbar icon size: 0 smol, 1 Normal, 2 large
+            $TweakInfo = [ordered]@{
+                Name        =  [IO.Path]::GetFileNameWithoutExtension($_.BaseName)
+                Description = $Tweak.Description     
+                Path        = $_.FullName           
+            }
 
-# taskbar search settings
-$Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search\'
-sp -path $Path -Name 'SearchboxTaskbarMode' -Value 0 -Type 'DWord'
+            if ($Detailed.IsPresent) {
+                $TweakInfo['Keys'] = $Tweak.Keys
+            }
 
-# input language switching - set all to none
-$Path = 'HKCU:\Keyboard Layout\Toggle\'
-sp -path $Path -Name 'Hotkey' -Value 3 -Type 'String' 
-sp -path $Path -Name 'Language Hotkey' -Value 3 -Type 'String' 
-sp -path $Path -Name 'Layout Hotkey' -Value 3 -Type 'String'
+            [PSCustomObject]$TweakInfo
+        }
+    }
+    'Default' {
+        $TweakList | ForEach-Object {
+            $Name =  [IO.Path]::GetFileNameWithoutExtension($_.BaseName)
+            $ApplyTweak = $Include.Length -eq 0 -or $Include -contains $Name
+            $ApplyTweak = $ApplyTweak -and ($Exclude.Length -eq 0 -or $Exclude -notcontains $Name)
+            Write-Verbose "$($Name) : $ApplyTweak" -Verbose
+            
+            if ($ApplyTweak) {
+                $Tweak = Get-Content -Raw $_.FullName | ConvertFrom-Json
+                $Tweak.Keys | ForEach-Object {
+                    $Params = @{
+                        Path  = $_.Path
+                        Name  = $_.Name
+                        Value = $_.Value
+                        Type  = $_.Type
+                    }
+                    
+                    Set-ItemProperty @Params
+                }
+            }
+        }
+    }
+    Default {
+        throw "Unknown paramater set: $($PSCmdlet.ParameterSetName)"
+    }
+}
 
-
-# Default Terminal Application  
-# see https://support.microsoft.com/en-gb/windows/command-prompt-and-windows-powershell-for-windows-11-6453ce98-da91-476f-8651-5c14d5777c20
-$Path = 'HKCU:\Console\%%Startup\'
-sp -path $Path -Name 'DelegationConsole' -Value '{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}' -Type 'String' 
-sp -path $Path -Name 'DelegationTerminal' -Value '{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}' -Type 'String' 
 
