191a59d3ee002876b2902fac3f0f301259449288
steven <you@example.com>
2024-01-02 12:43:57 -0500

    updated lay.ps1
    - Added argument completion for *Include* & *Exclude* params

--- slabs/reg-tweaks/lay.ps1
+++ slabs/reg-tweaks/lay.ps1
@@ -1,9 +1,11 @@
 
 param (
     [Parameter(ParameterSetName = "Default", Mandatory = $false)]
+    [ArgumentCompletions('DarkMode', 'DefaultTerminal', 'NoLanguageInputSwitching', 'TidyTaskBar')]
     [string[]]$Include = @(),
 
     [Parameter(ParameterSetName = "Default", Mandatory = $false)]
+    [ArgumentCompletions('DarkMode', 'DefaultTerminal', 'NoLanguageInputSwitching', 'TidyTaskBar')]
     [string[]]$Exclude = @(),
 
     [Parameter(ParameterSetName = "ListAvailable", Mandatory = $true)]
@@ -18,57 +20,62 @@ param (
 
 )
 
-$ErrorActionPreference = 'Stop'
-$InformationPreference = 'Continue'
+begin {
+    $ErrorActionPreference = 'Stop'
+    $InformationPreference = 'Continue'
+}
 
-$SlabsRoot = (Resolve-Path(Join-Path $PSScriptRoot '..')).Path
-. "$SlabsRoot/slab-utils/slab-utils.ps1"
+process {
 
-$TweakList = Get-ChildItem "$TweaksPath\*.reg-tweaks.json"
-
-switch ($PSCmdlet.ParameterSetName) {
-    'ListAvailable' {
-        $TweakList | ForEach-Object {
-            $Tweak = Get-Content -Raw $_.FullName | ConvertFrom-Json
-
-            $TweakInfo = [ordered]@{
-                Name        =  [IO.Path]::GetFileNameWithoutExtension($_.BaseName)
-                Description = $Tweak.Description     
-                Path        = $_.FullName           
-            }
-
-            if ($Detailed.IsPresent) {
-                $TweakInfo['Keys'] = $Tweak.Keys
-            }
-
-            [PSCustomObject]$TweakInfo
-        }
-    }
-    'Default' {
-        $TweakList | ForEach-Object {
-            $Name =  [IO.Path]::GetFileNameWithoutExtension($_.BaseName)
-            $ApplyTweak = $Include.Length -eq 0 -or $Include -contains $Name
-            $ApplyTweak = $ApplyTweak -and ($Exclude.Length -eq 0 -or $Exclude -notcontains $Name)
-            Write-Verbose "$($Name) : $ApplyTweak" -Verbose
-            
-            if ($ApplyTweak) {
+    $SlabsRoot = (Resolve-Path(Join-Path $PSScriptRoot '..')).Path
+    . "$SlabsRoot/slab-utils/slab-utils.ps1"
+    
+    $TweakList = Get-ChildItem "$TweaksPath\*.reg-tweaks.json"
+    
+    switch ($PSCmdlet.ParameterSetName) {
+        'ListAvailable' {
+            $TweakList | ForEach-Object {
                 $Tweak = Get-Content -Raw $_.FullName | ConvertFrom-Json
-                $Tweak.Keys | ForEach-Object {
-                    $Params = @{
-                        Path  = $_.Path
-                        Name  = $_.Name
-                        Value = $_.Value
-                        Type  = $_.Type
-                    }
+                
+                $TweakInfo = [ordered]@{
+                    Name        = [IO.Path]::GetFileNameWithoutExtension($_.BaseName)
+                    Description = $Tweak.Description     
+                    Path        = $_.FullName           
+                }
+
+                if ($Detailed.IsPresent) {
+                    $TweakInfo['Keys'] = $Tweak.Keys
+                }
+
+                [PSCustomObject]$TweakInfo
+            }
+        }
+        'Default' {
+            $TweakList | ForEach-Object {
+                $Name = [IO.Path]::GetFileNameWithoutExtension($_.BaseName)
+                $ApplyTweak = $Include.Length -eq 0 -or $Include -contains $Name
+                $ApplyTweak = $ApplyTweak -and ($Exclude.Length -eq 0 -or $Exclude -notcontains $Name)
+                Write-Verbose "$($Name) : $ApplyTweak" -Verbose
+            
+                if ($ApplyTweak) {
+                    $Tweak = Get-Content -Raw $_.FullName | ConvertFrom-Json
+                    $Tweak.Keys | ForEach-Object {
+                        $Params = @{
+                            Path  = $_.Path
+                            Name  = $_.Name
+                            Value = $_.Value
+                            Type  = $_.Type
+                        }
                     
-                    Set-ItemProperty @Params
+                        Set-ItemProperty @Params
+                    }
                 }
             }
         }
+        Default {
+            throw "Unknown paramater set: $($PSCmdlet.ParameterSetName)"
+        }
     }
-    Default {
-        throw "Unknown paramater set: $($PSCmdlet.ParameterSetName)"
-    }
-}
 
+}
 
