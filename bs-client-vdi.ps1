325f59c22e064cd088a89a12c7f38b9fa052639c
Steven Rose <stvnrs@users.noreply.github.com>
2024-09-26 14:33:07 +0100

    Update bs-client-vdi.ps1

--- bs-client-vdi.ps1
+++ bs-client-vdi.ps1
@@ -12,9 +12,13 @@ Import-Module pave
 Install-Slab slab-utils
 Install-Slab bs-no-admin
 Install-Slab reg-tweaks
-lay bs-no-admin -PwshVersion '7.4.4'
+lay bs-no-admin -PwshVersion '7.4.5'
 winget install Python.Python.3.12
-git clone https://github.com/stvnrs/config
-./config/configs/uwm-vm/doit.ps1
+
+{
+    git clone https://github.com/stvnrs/config
+    ./config/configs/uwm-vm/doit.ps1
+} | & "$Env:LocalAppData\powershell\pwsh" -WorkingDirectory $PSScriptRoot -noexit  -command - # note the sneaky - at the end!
+
 lay reg-tweaks
 
