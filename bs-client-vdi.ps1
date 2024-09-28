98cd793e1cb69c30b5a474b3d6a409d2857db118
Steven Rose <stvnrs@users.noreply.github.com>
2024-09-28 14:46:47 +0100

    Update bs-client-vdi.ps1

--- bs-client-vdi.ps1
+++ bs-client-vdi.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = 'true'
 
-$Env:PAVE_PWSH_VERSION = 7.4.5
-$Env:PAVE_PY_VERSION = 3.12
+$Env:PAVE_PWSH_VERSION = '7.4.5'
+$Env:PAVE_PY_VERSION = '3.12'
 
 if($null -eq $Env:PAVE_USER_NAME){
     $Env:PAVE_USER_NAME = Read-Host -Prompt "Enter your user name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
