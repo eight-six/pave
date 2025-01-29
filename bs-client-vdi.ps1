c389eefbb029830b649129930402f9f51e0d0a09
Steven ROSE <steven@eightsix.io>
2025-01-29 00:07:21 +0000

    refactored bs-client-vdi.ps1

--- bs-client-vdi.ps1
+++ bs-client-vdi.ps1
@@ -3,30 +3,65 @@ $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = 'true'
 
 $Env:PAVE_PWSH_VERSION = '7.5.0'
+$Env:PAVE_REMOTE = "https://eightsixpaveprodstg.blob.core.windows.net/public/latest-test"
+$Env:PAVE_PY_VERSION = '3.12|3.11' # separate multiple versions with a | - versions are installed left to right, the last one will be the default.
 
-# separate multiple versions with a | - versions are installed left to right, the last one will be the default.
-$Env:PAVE_PY_VERSION = '3.12|3.11'
-
-if($null -eq $Env:PAVE_USER_NAME){
+if ($null -eq $Env:PAVE_USER_NAME) {
     $Env:PAVE_USER_NAME = Read-Host -Prompt "Enter your user name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
 } 
 
-if($null -eq $Env:PAVE_USER_EMAIL ){
+if ($null -eq $Env:PAVE_USER_EMAIL ) {
     $Env:PAVE_USER_EMAIL = Read-Host -Prompt "Enter your email name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
 } 
 
 Set-ExecutionPolicy 'RemoteSigned' -Scope 'CurrentUser'
 
-cd "$HOME\downloads" 
+function prompt {        
+    $Dollar = $IsAdmin ? '#' : '$'
+    $Color = $IsAdmin ? $PSStyle.Formatting.Error : $PSStyle.Formatting.White
+    $Options = Get-PSReadLineOption
+
+    $Line1 = @(
+        $Options.CommentColor
+        $Pwd.Path.Replace($HOME, '~')
+        $PSStyle.Reset
+    ) -join ''
+
+    $Line2 = "$($Color)P$Dollar $($PSStyle.Reset)"
+
+    '', $Line1, $Line2 -join "`n"
+}
+
+$InstallCachePath = "$HOME\downloads\~pave" 
+
+if (!(Test-Path $InstallCachePath )) {
+    md $InstallCachePath | Out-Null
+}
+
+cd $InstallCachePath  
+
 $ModulePath = "$($env:PSModulePath -split ';' | select -First 1)"
-$ModuleZipFileName = 'pave-full-v99.99.99.zip'
-Start-BitsTransfer "https://eightsixpaveprodstg.blob.core.windows.net/public/latest-test/$ModuleZipFileName" 
+$PackageBaseName = 'pave-full-v99.99.99'
+$ModuleZipFileName = "$PackageBaseName.zip"
+
+if (Test-Path $ModuleZipFileName ) {
+    rm $ModuleZipFileName | Out-Null
+}
+
+'pave-logger', 'pave-utils', 'pave' | % {
+    if (Test-Path "$ModulePath\$_") {
+        rm "$ModulePath\$_" -Recurse -Force | Out-Null
+    }
+}
+
+Start-BitsTransfer "$Env:PAVE_REMOTE/$ModuleZipFileName" 
+Expand-Archive $ModuleZipFileName
 Expand-Archive './pave-full-v99.99.99/pave-logger-module-v99.99.99.zip' $ModulePath
 Expand-Archive './pave-full-v99.99.99/pave-utils-module-v99.99.99.zip' $ModulePath
 Expand-Archive './pave-full-v99.99.99/pave-module-v99.99.99.zip' $ModulePath
 rm $ModuleZipFileName 
 
-if(!(Get-Module -ListAvailable 'powershell-yaml')){
+if (!(Get-Module -ListAvailable 'powershell-yaml')) {
     Install-Module powershell-yaml -Scope 'CurrentUser' -Force
 }
 
@@ -34,10 +69,11 @@ Import-Module pave-logger
 Import-Module pave-utils
 Import-Module pave
 
+Set-Remote $Env:PAVE_REMOTE
 Install-Slab slab-utils
 Install-Slab bs-no-admin
 Install-Slab reg-tweaks
-lay bs-no-admin -PwshVersion $Env:PAVE_PWSH_VERSION 
+lay bs-no-admin -PwshVersion $Env:PAVE_PWSH_VERSION
 
 $Env:PAVE_PY_VERSION -split '\|' | % {
     winget install "Python.Python.$_" --accept-source-agreements 
@@ -45,60 +81,35 @@ $Env:PAVE_PY_VERSION -split '\|' | % {
     $Env:Path = "$Env:LOCALAPPDATA\Programs\Python\Python$PyVersion;$Env:LOCALAPPDATA\Programs\Python\Python$PyVersion\Scripts;" + $Env:Path
 }
 
-# When copy & pasted or directly downloaded line endings in this "file" are LF.  This causes
-# some interesting issues.  Hence, write script to file and execute that.
-@'
-    $ErrorActionPreference = 'Stop'
-    
-    if($null -eq (gcm git -ea 'Ignore')){
-        $Env:Path = "$Env:LOCALAPPDATA\Programs\git\bin;" + $Env:Path
-        }
-        
-    $Uri = 'https://raw.githubusercontent.com/eight-six/pave/refs/heads/main/slabs/bs-no-admin/scripts/Install-Node.ps1'
-    Start-BitsTransfer $Uri
-    # the previous command downloads the raw file for github which has LF line endings - this causes issues with PowerShell 5.1 and 7.4.6
-    # cat'ing the file replaces the LFs with CR+LFs
-    cat .\Install-Node.ps1 > .\Install-NodeWin.ps1
-    & .\Install-NodeWin.ps1
-'@ | Out-File -Encoding utf8 ./bs-node.ps1
+function Invoke-ScriptWithPwsh {
+    param(
+        [string]$FilePath
+    )
 
- & "$Env:LocalAppData\powershell\pwsh" -file ./bs-node.ps1
- 
-{
-    $ErrorActionPreference = 'Stop'
-    $PSNativeCommandUseErrorActionPreference = $true
+    $PwshPath = "$Env:LocalAppData\powershell\$Env:PAVE_PWSH_VERSION\pwsh"
+    & $PwshPath -NoProfile -File $FilePath 
     
-    if($Env:Path[-1] -ne ';'){
-        $Env:Path += ';'
+    if ($LASTEXITCODE -ne 0) {
+        $ErrorMessage = "Running $(em $FilePath ) with $(em $PwshPath ) failed with exit code $(em $LASTEXITCODE)."
+        Write-LogEntry $ErrorMessage -IgnoreActionLevel
+        throw $ErrorMessage
     }
+}
 
-    if($null -eq (gcm git -ea 'Ignore')){
-        $Env:Path = "$Env:LOCALAPPDATA\Programs\git\bin;" + $Env:Path
-    }
-    
-    if($null -eq (gcm code-insiders -ea 'Ignore')){
-        $Env:Path += "$Env:LOCALAPPDATA\Programs\Microsoft VS Code Insiders\bin;"
-    }
-    
-    $Proxy = [System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://github.com')
-    $AddtionalArgs = if($null -ne $Proxy){"-c http.proxy=$($Proxy.OriginalString)"}else{''}
-    git clone $AddtionalArgs https://github.com/stvnrs/config
-    & ./config/configs/uwm-vm/doit.ps1
-} | & "$Env:LocalAppData\powershell\pwsh" -command - # note the sneaky '-' at the end!
+$PwshPath = "$Env:LocalAppData\powershell\$Env:PAVE_PWSH_VERSION\pwsh"
+$ScriptsPath = "$(Get-Cache)\bs-no-admin\scripts"
 
-{
-    $ErrorActionPreference = 'Stop'
-    $PSNativeCommandUseErrorActionPreference = $true
-    
-    if($null -eq (gcm git -ea 'Ignore')){
-        $Env:Path = "$Env:LOCALAPPDATA\Programs\git\bin;" + $Env:Path
-    }
+# install python versions
+$InstallPythonFilePath = Join-Path $ScriptsPath 'Install-PythonWinget.ps1'
+Invoke-ScriptWithPwsh $InstallPythonFilePath 
 
-    $Proxy = [System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://github.com')
-    $AddtionalArgs = if($null -ne $Proxy){"-c http.proxy=$($Proxy.OriginalString)"}else{''}
-    git clone  $AddtionalArgs  https://github.com/stvnrs/config-private
-    . ./config-private/configs/uwm-vm/env/doit.ps1
-    . ./config-private/configs/uwm-vm/pwsh/doit.ps1
-    & ./config-private/configs/uwm-vm/code/doit.ps1
+# install node
+$InstallNodeFilePath = Join-Path $ScriptsPath 'Install-Node.ps1'
+Invoke-ScriptWithPwsh $InstallNodeFilePath 
 
-} | & "$Env:LocalAppData\powershell\pwsh" -command - # note the sneaky '-' at the end!
+# apply configs
+$ConfigFilePath = Join-Path $ScriptsPath 'Set-ConfigConfig.ps1'
+Invoke-ScriptWithPwsh $ConfigFilePath 
+$ConfigFilePath = Join-Path $ScriptsPath 'Set-ConfigPrivate.ps1'
+Invoke-ScriptWithPwsh$ConfigFilePath 
+
