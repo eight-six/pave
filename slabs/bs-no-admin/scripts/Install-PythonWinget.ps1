62c59340c801dc6cb60d070267f43f120eabda6a
Steven ROSE <steven@eightsix.io>
2025-01-30 10:20:08 +0000

    Updated slabs\bs-no-admin\scripts\Install-PythonWinget.ps1

--- slabs/bs-no-admin/scripts/Install-PythonWinget.ps1
+++ slabs/bs-no-admin/scripts/Install-PythonWinget.ps1
@@ -1,7 +1,48 @@
+#Requires -PSEdition Core
+#Requires -Modules pave-logger
 
+$ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
+
+$Heading = 'python'
+Write-LogHeader $Heading -Subheader:($null -ne $MyInvocation.PSCommandPath)
+Push-LogAction "installing dotnet latest lts" -IncrementActionLevel
 
 $Env:PAVE_PY_VERSION -split '\|' | % {
+    Push-LogAction "Installing python $_"
     winget install "Python.Python.$_" --accept-source-agreements 
+    Push-LogAction "Adding python $_"
+    
     $PyVersion = $_ -replace '\.', ''
-    $Env:Path = "$Env:LOCALAPPDATA\Programs\Python\Python$PyVersion;$Env:LOCALAPPDATA\Programs\Python\Python$PyVersion\Scripts;" + $Env:Path
+    $PyPath = "$Env:LOCALAPPDATA\Programs\Python\Python$PyVersion" 
+    $PyScriptsPath = Join-Path $PyPath 'Scripts'
+
+    Pop-LogAction "Adding $(em $PyPath) to Path"
+    Add-UserPath $PyPath -AtStart -AddToCurrentSession
+    Pop-LogAction
+
+    Pop-LogAction "Adding $(em $PyScriptsPath) to Path" 
+    Add-UserPath $PyScriptsPath -AtStart -AddToCurrentSession
+    Pop-LogAction
+}
+
+# remove Windows App Store 'shims' for python
+Push-LogAction 'Removing windows app shims for python'
+
+$WindowsAppsRoot = "$Env:LOCALAPPDATA\Microsoft\WindowsApps"
+$WindowsStorePythonPaths = "$WindowsAppsRoot\python.exe", "$WindowsAppsRoot\python3.exe"
+
+$WindowsStorePythonPaths | ForEach-object {
+    if (Test-Path $_){
+        Push-LogAction "Removing shim $(em $_)"
+        Remove-Item -path $_ -Force
+        Pop-LogAction
+    }
+}
+
+Pop-LogAction
+
+if($null -eq $MyInvocation.PSCommandPath){
+	$Heading += ' - completed'
+	Write-LogHeader $Heading 
 }
