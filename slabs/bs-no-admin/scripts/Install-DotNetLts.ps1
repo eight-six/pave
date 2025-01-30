f0ed016ffa2475f397890f57762a236914b52cc6
Steven ROSE <steven@eightsix.io>
2025-01-30 12:29:02 +0000

    Removed duplicate section in slabs\bs-no-admin\scripts\Install-DotNetLts.ps1

--- slabs/bs-no-admin/scripts/Install-DotNetLts.ps1
+++ slabs/bs-no-admin/scripts/Install-DotNetLts.ps1
@@ -8,10 +8,6 @@ $Heading = 'dotnet'
 Write-LogHeader $Heading -Subheader:($null -ne $MyInvocation.PSCommandPath)
 Push-LogAction "installing dotnet latest lts" -IncrementActionLevel
 
-$Heading = 'dotnet'
-Write-LogHeader $Heading -Subheader:($null -ne $MyInvocation.PSCommandPath)
-Push-LogAction "installing dotnet latest lts" -IncrementActionLevel
-
 $DotNetInstallUri = 'https://dot.net/v1/dotnet-install.ps1'
 $DownloadFilePath = Join-Path $pwd.path 'dotnet-install.ps1' 
 Push-LogAction "Downloading install script from $(emph $DotNetInstallUri) to $(emph $DownloadFilePath)"
