883062442fd9644321cfed4376f1a469596f7152
Steven ROSE <steven@eightsix.io>
2025-02-08 13:26:35 +0000

    Updated slabs\bs-no-admin\scripts\Install-Apps.ps1
    - removed dotnet lts install

--- slabs/bs-no-admin/scripts/Install-Apps.ps1
+++ slabs/bs-no-admin/scripts/Install-Apps.ps1
@@ -10,7 +10,6 @@ $VsBuildType = 'insider'
 $Heading = 'default apps'
 Write-LogHeader $Heading -Subheader:($null -ne $MyInvocation.PSCommandPath)
 Push-LogAction 'installing default apps' -IncrementActionLevel
-& "$ScriptsFolder\Install-DotNetLts.ps1"
 & "$ScriptsFolder\Install-GitForWindows.ps1" 
 & "$ScriptsFolder\Install-BsCode.ps1" -BuildType $VsBuildType
 & "$ScriptsFolder\Install-AzureDataStudio.ps1"
