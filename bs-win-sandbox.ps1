2e46e6c3b19b271046e8eab480f8fa19073a87d7
Steven ROSE <steven@eightsix.io>
2025-02-23 11:24:42 +0000

    updated bs-win-sandbox.ps1
    - install pave modules
    - install default apps with winget

--- bs-win-sandbox.ps1
+++ bs-win-sandbox.ps1
@@ -1,8 +1,8 @@
 <#
 
-Set-ExecutionPolicy 'RemoteSigned' -Scope 'CurrentUser';
-$FileName = 'bs-win-sandbox.ps1'
-$FilePath = "~\downloads\$FileName"
+Set-ExecutionPolicy 'RemoteSigned' -Scope 'CurrentUser' -Force;
+$FileName = 'bs-win-sandbox.ps1';
+$FilePath = "~\downloads\$FileName";
 iwr "https://raw.githubusercontent.com/eight-six/pave/refs/heads/tidy/$FileName" -OutFile $FilePath;
 Unblock-File $FilePath;
 . $FilePath;
@@ -13,7 +13,9 @@ Unblock-File $FilePath;
 
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
+$PSNativeCommandUseErrorActionPreference = 'true'
 
+#region support functions
 function Log {
     param(
         [string] $message
@@ -22,10 +24,43 @@ function Log {
     Write-Information "$(get-date -format 'yyyy-MM-ddTHH:mm:ssZ') $message"
 }
 
+function prompt {        
+    $Role = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $IsAdmin = (New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole($role)
+    $Dollar = if($IsAdmin ){'♆'} else {'P$'}
+    $Options = Get-PSReadLineOption
+    $Color = if($IsAdmin){$Options.ErrorColor } else {$Options.DefaultTokenColor}
+
+    $Line1 = @(
+        $Options.CommentColor
+        $Pwd.Path.Replace($HOME, '~')
+        $PSStyle.Reset
+    ) -join ''
+
+    $Line2 = "$($Color)$Dollar $($PSStyle.Reset)"
+
+    '', $Line1, $Line2 -join "`n"
+}
+#endregion
+
+#region config
 $NugetMinVersion = '2.8.5.201'
+$Env:PAVE_PWSH_VERSION = '7.5.0'
+$Env:PAVE_REMOTE = "https://eightsixpaveprodstg.blob.core.windows.net/public/latest-test"
+$Env:PAVE_PY_VERSION = '3.12|3.11' # separate multiple versions with a | - versions are installed left to right, the last one will be the default.
+
+if ($null -eq $Env:PAVE_USER_NAME) {
+    $Env:PAVE_USER_NAME = Read-Host -Prompt "Enter your user name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
+} 
+
+if ($null -eq $Env:PAVE_USER_EMAIL ) {
+    $Env:PAVE_USER_EMAIL = Read-Host -Prompt "Enter your email name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
+}
+#endregion
 
 Set-ExecutionPolicy -ExecutionPolicy 'RemoteSigned' -Scope 'CurrentUser' -Force
 
+#region register ps gallery and install winget
 if($null -eq (Get-PackageProvider | ? { ($_.Name -eq 'NuGet') -and ($_.Version -ge $NugetMinVersion)})){
     Log "Installing nuget $NugetMinVersion or later..."
     Install-PackageProvider -Name 'NuGet' -MinimumVersion $NugetMinVersion -Scope 'CurrentUser' -Force 
@@ -46,3 +81,48 @@ Log "Installing winget..."
 Install-Module -Name 'Microsoft.WinGet.Client'  -Repository 'PSGallery' -Force -Scope 'CurrentUser'
 Repair-WingetPackageManager
 Log "Installing winget - done"
+#endregion
+
+$InstallCachePath = "$HOME\downloads\~pave" 
+
+if (!(Test-Path $InstallCachePath )) {
+    md $InstallCachePath | Out-Null
+}
+
+cd $InstallCachePath  
+
+$ModulePath = "$($env:PSModulePath -split ';' | select -First 1)"
+$PackageBaseName = 'pave-full-v99.99.99'
+$ModuleZipFileName = "$PackageBaseName.zip"
+
+if (Test-Path $ModuleZipFileName ) {
+    rm $ModuleZipFileName | Out-Null
+}
+
+'pave-logger', 'pave-utils', 'pave' | % {
+    if (Test-Path "$ModulePath\$_") {
+        rm "$ModulePath\$_" -Recurse -Force | Out-Null
+    }
+}
+
+Start-BitsTransfer "$Env:PAVE_REMOTE/$ModuleZipFileName" 
+Expand-Archive $ModuleZipFileName
+Expand-Archive './pave-full-v99.99.99/pave-logger-module-v99.99.99.zip' $ModulePath
+Expand-Archive './pave-full-v99.99.99/pave-utils-module-v99.99.99.zip' $ModulePath
+Expand-Archive './pave-full-v99.99.99/pave-module-v99.99.99.zip' $ModulePath
+rm $ModuleZipFileName 
+
+if (!(Get-Module -ListAvailable 'powershell-yaml')) {
+    Install-Module powershell-yaml -Scope 'CurrentUser' -Force
+}
+
+Import-Module pave-logger
+Import-Module pave-utils
+Import-Module pave
+
+Set-Remote $Env:PAVE_REMOTE
+Install-Slab slab-utils
+Install-Slab bs-no-admin
+Install-Slab reg-tweaks
+lay bs-no-admin -PwshVersion $Env:PAVE_PWSH_VERSION -UseWinget
+Update-PathEnvVar 
