6c3105035732feac3c89eead0f0b2edf92394b5e
stvnrs <steven@eightsix.io>
2025-03-07 09:06:04 +0000

    Added bs-win-sandbox-lite.ps1

---
+++ bs-win-sandbox-lite.ps1
@@ -0,0 +1,185 @@
+<#
+
+Set-ExecutionPolicy 'RemoteSigned' -Scope 'CurrentUser' -Force;
+$Branch = 'tidy';
+$FileName = 'bs-win-sandbox.ps1';
+$FilePath = "~\downloads\$FileName";
+iwr "https://raw.githubusercontent.com/eight-six/pave/refs/heads/$Branch/$FileName" -OutFile $FilePath;
+Unblock-File $FilePath;
+. $FilePath;
+
+#>
+
+#requires -Modules PowershellGet
+
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+$PSNativeCommandUseErrorActionPreference = 'true'
+
+#region config
+$NugetMinVersion = '2.8.5.201'
+$Env:PAVE_PWSH_VERSION = '7.5.0'
+$Env:PAVE_REMOTE = "https://eightsixpaveprodstg.blob.core.windows.net/public/latest-test"
+$Env:PAVE_PY_VERSION = '3.12|3.11' # separate multiple versions with a | - versions are installed left to right, the last one will be the default.
+$AddtionalApps = @()
+
+if ($null -eq $Env:PAVE_USER_NAME) {
+    $Env:PAVE_USER_NAME = Read-Host -Prompt "Enter your user name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
+} 
+
+if ($null -eq $Env:PAVE_USER_EMAIL ) {
+    $Env:PAVE_USER_EMAIL = Read-Host -Prompt "Enter your email name for git logs (set `$Env:PAVE_USER_NAME to avoid this prompt in future)"
+}
+#endregion
+
+#region support functions
+function Log {
+    param(
+        [string] $message
+    )
+
+    Write-Information "$(get-date -format 'yyyy-MM-ddTHH:mm:ssZ') $message"
+}
+
+function prompt {        
+    $Role = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $IsAdmin = (New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole($role)
+    $Dollar = if ($IsAdmin ) { '♆' } else { 'P$' }
+    $Options = Get-PSReadLineOption
+    $Color = if ($IsAdmin) { $Options.ErrorColor } else { $Options.DefaultTokenColor }
+
+    $Line1 = @(
+        $Options.CommentColor
+        $Pwd.Path.Replace($HOME, '~')
+        $PSStyle.Reset
+    ) -join ''
+
+    $Line2 = "$($Color)$Dollar $($PSStyle.Reset)"
+
+    '', $Line1, $Line2 -join "`n"
+}
+
+function Invoke-Pwsh {
+    # [CmdletBinding(DefaultParameterSetName = 'WithFile')]
+    param(
+
+        [Parameter(ParameterSetName = 'WithFile', Mandatory, Position=0)]
+        [string]$FilePath,
+        [Parameter(ParameterSetName = 'WithScriptBlock', Mandatory)]
+        [string]$ScriptBlock,
+        [Parameter(ParameterSetName = 'WithCommand', Mandatory)]
+        [string]$Command
+    )
+
+    $PwshPath = "$Env:LocalAppData\powershell\$Env:PAVE_PWSH_VERSION\pwsh"
+
+    if ($ScriptBlock.IsPresent) {
+        & $PwshPath -NoProfile -Command $ScriptBlock 
+    }
+    if ($WithCommand.IsPresent) {
+        & $PwshPath -NoProfile -Command $Command
+    }
+    else {
+        & $PwshPath -NoProfile -File $FilePath 
+    }
+    if ($LASTEXITCODE -ne 0) {
+        $ErrorMessage = "Running $(em $FilePath ) with $(em $PwshPath ) failed with exit code $(em $LASTEXITCODE)."
+        Write-LogEntry $ErrorMessage -IgnoreActionLevel
+        throw $ErrorMessage
+    }
+
+    Update-PathEnvVar 
+
+}
+#endregion 
+
+Set-ExecutionPolicy -ExecutionPolicy 'RemoteSigned' -Scope 'CurrentUser' -Force
+
+#region install winget
+if ($null -eq (Get-PackageProvider | ? { ($_.Name -eq 'NuGet') -and ($_.Version -ge $NugetMinVersion) })) {
+    Log "Installing nuget $NugetMinVersion or later..."
+    Install-PackageProvider -Name 'NuGet' -MinimumVersion $NugetMinVersion -Scope 'CurrentUser' -Force 
+    Log "Installing nuget $NugetMinVersion or later - done!"
+}
+else {
+    Log "Nuget already installed :)"
+}
+
+if ($null -eq (Get-PSRepository | ? SourceLocation -eq 'https://www.powershellgallery.com/api/v2' )) {
+    Log "Registering PS Gallery..."
+    Register-PSRepository -Default -Force
+    Log "Registering PS Gallery - done!"
+}
+else {
+    Log "PS Gallery already registered :)"
+}
+
+Log "Installing winget..."
+Install-Module -Name 'Microsoft.WinGet.Client'  -Repository 'PSGallery' -Force -Scope 'CurrentUser'
+Repair-WingetPackageManager
+Log "Installing winget - done"
+#endregion
+
+#region install pave
+$InstallCachePath = "$HOME\downloads\~pave" 
+
+if (!(Test-Path $InstallCachePath )) {
+    md $InstallCachePath | Out-Null
+}
+
+cd $InstallCachePath  
+
+$ModulePath = "$($env:PSModulePath -split ';' | select -First 1)"
+$PackageBaseName = 'pave-full-v99.99.99'
+$ModuleZipFileName = "$PackageBaseName.zip"
+
+if (Test-Path $ModuleZipFileName ) {
+    rm $ModuleZipFileName | Out-Null
+}
+
+'pave-logger', 'pave-utils', 'pave' | % {
+    if (Test-Path "$ModulePath\$_") {
+        rm "$ModulePath\$_" -Recurse -Force | Out-Null
+    }
+}
+
+Start-BitsTransfer "$Env:PAVE_REMOTE/$ModuleZipFileName" 
+Expand-Archive $ModuleZipFileName
+Expand-Archive './pave-full-v99.99.99/pave-logger-module-v99.99.99.zip' $ModulePath
+Expand-Archive './pave-full-v99.99.99/pave-utils-module-v99.99.99.zip' $ModulePath
+Expand-Archive './pave-full-v99.99.99/pave-module-v99.99.99.zip' $ModulePath
+rm $ModuleZipFileName 
+
+if (!(Get-Module -ListAvailable 'powershell-yaml')) {
+    Install-Module powershell-yaml -Scope 'CurrentUser' -Force
+}
+
+Import-Module pave-logger
+Import-Module pave-utils
+Import-Module pave
+
+Set-Remote $Env:PAVE_REMOTE
+Install-Slab slab-utils
+Install-Slab bs-no-admin
+Install-Slab reg-tweaks
+#endregion
+
+#region install default apps
+lay bs-no-admin -PwshVersion $Env:PAVE_PWSH_VERSION -UseWinget
+Update-PathEnvVar 
+#endregion
+
+#region install additional apps
+$ScriptsPath = "$(Get-Cache)\bs-no-admin\scripts"
+
+'WindowsTerminal' | % {
+    $InstallFilePath = Join-Path $ScriptsPath "Install-$_.ps1"
+    Invoke-Pwsh -File $InstallFilePath 
+}
+
+#region apply configs
+$ConfigFilePath = Join-Path $ScriptsPath 'Set-Config.ps1'
+$SharedParams = "-Org 'stvnrs' -Repo 'config' -Path 'uwm-vm'"  
+Invoke-Pwsh -command "$ConfigFilePath $SharedParams -DotSource -Include 'env'"
+Invoke-Pwsh -command "$ConfigFilePath $SharedParams -Exclude 'env'"
+#endregion
